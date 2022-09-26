9c6a5239fe558b6206e3f5605af9a2b0f7d69728
Ebenezr <ebenezarbukosia@gmail.com>
2022-09-26 14:33:37 +0300

    commit tree 223f5d9564fd8af3bbceedcf71010552792f8160

---
+++ app/models/create_table_student.rb
@@ -0,0 +1,2 @@
+class CreateTableStudent < ApplicationRecord
+end
---
+++ db/migrate/20220926113239_create_create_table_students.rb
@@ -0,0 +1,11 @@
+class CreateCreateTableStudents < ActiveRecord::Migration[6.1]
+  def change
+    create_table :create_table_students do |t|
+      t.string :first_name
+      t.string :last_name
+      t.integer :grade
+
+      t.timestamps
+    end
+  end
+end
